a7c68a117a4e871bd0c3a6d6763738a2a13c213a
PoojaHasda <141259541+PoojaHasda@users.noreply.github.com>
2024-09-06 22:03:23 +0530

    first commit

---
+++ src/main/java/com/contactmanager/contact/controllers/PageController.java
@@ -0,0 +1,53 @@
+package com.contactmanager.contact.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class PageController {
+
+    @GetMapping("/home")
+
+    public String home(Model model){
+        model.addAttribute("name", "Spring Boot");
+        System.out.println("Home page handler");
+        return "home";
+    }
+
+
+    // route for about
+    @GetMapping("/about")
+
+    public String aboutPage(){
+        return new String("about");
+    }
+
+    @GetMapping("/service")
+
+    public String servicePage(Model model){
+        model.addAttribute("name", "Spring Boot");
+        System.out.println("Home page handler");
+        return "service";
+    }
+
+    @GetMapping("/contact")
+    public String getContactPage(){
+        return new String("contact");
+    }
+
+    @GetMapping("/login")
+    public String getloginPage() {
+        return new String("login");
+    }
+
+    
+    @GetMapping("/register")
+    public String getSignUpPage() {
+        return new String("register");
+    }
+    
+
+}
